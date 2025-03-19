4e21471944b22800465ece8f7384e759c3ff8e7f
Michael Horgan <michaelghorgan@gmail.com>
2025-03-19 15:04:22 -0700

    fix date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,24 +43,31 @@
 			<div class="section__list">
 				<div class="section__list-item">
 					<div class="left">
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://chessbot.michaelhorgan.me"
-							>An LLM-inspired ChessBot trained on human chess games</a
-						>
 						<div class="desc">
-							I trained a transformer neural network on human chess games from the <a
-								href="https://database.lichess.org"
+							<a
 								target="_blank"
-								rel="noopener noreferrer">Lichess database</a
-							> to play chess in the style of a human player. The training procedure mirrors
-							that of the "pre-training" phase for a Large Language Model.
+								rel="noopener noreferrer"
+								href="https://chessbot.michaelhorgan.me"
+								>An LLM-inspired ChessBot trained on human chess games</a
+							>
+							<div class="desc">
+								I trained a transformer neural network on human chess games from the <a
+									href="https://database.lichess.org"
+									target="_blank"
+									rel="noopener noreferrer">Lichess database</a
+								> to play chess in the style of a human player. The training procedure
+								mirrors that of the "pre-training" phase for a Large Language Model.
+							</div>
+							<a target="_blank" rel="noopener noreferrer" href="{base}/nhits-lorenz"
+								>Modeling Chaotic Dynamics with Deep Learning: A Case Study on the
+								Lorenz Attractor</a
+							>
 						</div>
-						<a target="_blank" rel="noopener noreferrer" href="{base}/nhits-lorenz"
-							>Modeling Chaotic Dynamics with Deep Learning: A Case Study on the
-							Lorenz Attractor</a
-						>
+					</div>
+					<div class="right"><div class="duration">Oct 2024 - Feb 2025</div></div>
+				</div>
+				<div class="section__list-item">
+					<div class="left">
 						<div class="desc">
 							I investigated the ability of a common neural forecasting architecture (<a
 								target="_blank"
